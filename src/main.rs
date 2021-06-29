39a473d4ff3bf7697f6d4804741dae9a03a37e22
Ayose <ayosec@gmail.com>
2021-06-29 16:09:18 +0100

    Copy printable ASCII characters, and escape everything else.
    Read from stdin and write to stdout.
    
    Escaped characters are printed in <XX> format.

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,33 @@
+use std::io::{self, BufWriter, Read, Write};
+use std::process;
+
 fn main() {
+    let mut buf = [0; 4096];
+
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+
+    let stdout = io::stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+
+    while let Ok(len) = reader.read(&mut buf) {
+        if len == 0 {
+            break;
+        }
+
+        for byte in &buf[..len] {
+            let res;
+
+            if (32..=128).contains(byte) || byte.is_ascii_whitespace() {
+                res = writer.write(&[*byte]).map(|_| ());
+            } else {
+                res = write!(&mut writer, "<{:02X}>", *byte);
+            }
+
+            if let Err(e) = res {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        }
+    }
 }
